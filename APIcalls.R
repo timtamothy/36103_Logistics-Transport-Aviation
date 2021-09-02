daa9fa57b9028991496acec57b51e21fec4cd622
Mandowara Devesh <dmandowara@woolworths.com.au>
2021-09-02 19:03:43 +1000

    Added Comments...First GIT commit :)

--- APIcalls.R
+++ APIcalls.R
@@ -15,10 +15,15 @@ library(jsonlite)
 #### With body:
 #### grant_type=clinet_credentials&client_id=API_Key&client_secret=API_Secret
 
+#### Get the Airport Data from ICAO API ----
 icao <- GET(
   url = 'https://applications.icao.int/dataservices/api/safety-characteristics-list?api_key=8d00ef90-0982-11ec-9d72-8160549d64ab&airports=&states=USA'
 )
+
+#### Get response JSON content ----
 response <- content(icao, 'parsed')
+
+#### Convert to data tibble from JSON ----
 data <- fromJSON(response)
 
 
