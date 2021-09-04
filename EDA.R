4bccfb378208129103adf58dd3426c58c5cdc386
timothywallaby <stevetimothy@gmail.com>
2021-09-04 20:19:46 +1000

    Distance, Elapsed Time Collinearity

--- EDA.R
+++ EDA.R
@@ -44,3 +44,11 @@ delays %>% # delay as a function of flight time
   filter(ARR_DELAY < 300) %>% 
   ggplot(aes(y=ARR_DELAY, x=ACTUAL_ELAPSED_TIME)) +
   geom_point() 
+
+delays %>% # may have collinearity
+  filter(ARR_DELAY < 300) %>% 
+  ggplot(aes(y=ACTUAL_ELAPSED_TIME, x=DISTANCE)) +
+  geom_point() 
+    #appears to have pretty strong collinearity?
+
+
