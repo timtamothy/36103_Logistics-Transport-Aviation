1662a0db6eed1db633815f579b6f4a222f9ba8a8
timothywallaby <stevetimothy@gmail.com>
2021-09-11 23:34:44 +1000

    Changed colour

--- datamerging.R
+++ datamerging.R
@@ -189,10 +189,10 @@ ontime3 %>%
   geom_boxplot(alpha = 0.3) +
   #ylim(0, 30) +
   theme_minimal() +
-  labs(title='Departure Delays in Q1 by Airline in 2020 and 2019') +
+  labs(title='Departure Delays in Q1 by Airline from 2019 to 2021') +
   ylab('Departure Delay in Minutes') +
   xlab('Airline') +
-  scale_fill_brewer(palette="Set2")
+  scale_fill_brewer(palette="PRGn")
 
 ontime3 %>% 
   filter(ontime3$DEP_DELAY >= 0 & ontime3$DEP_DELAY <= 60) %>%
