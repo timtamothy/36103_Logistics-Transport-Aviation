a0421d9734c9356e627cd7645495ca12dc88685d
timothywallaby <stevetimothy@gmail.com>
2021-09-24 20:37:41 +1000

    Join the airline fleet data

---
+++ merge_tailno_into_dataset.R
@@ -0,0 +1,35 @@
+# Load Packages ----
+library(tidyverse)
+library(here)
+library(feather)
+library(dplyr)
+library(janitor)
+
+# Load Files ----
+maindf <- read_feather(here('clean_3airlines.feather'))
+
+delta <- read_feather(here('DAL_fleet.feather'))
+
+american <- read_feather(here('AA_fleet.feather'))
+
+united <- read_feather(here('UAL_fleet.feather'))
+
+# Join Files ----
+
+allrows <- bind_rows(delta, american, united)
+
+testjoin <- left_join(maindf, allrows, by = c('tail_num' = 'Tail'))
+
+col_status <- status(testjoin)
+
+# Clean column names ----
+clean_join <- clean_names(testjoin)
+
+# Separate the age/year columns into two columns
+separate <- clean_join %>% 
+  separate(age_built, c('age', 'year'))
+
+status(separate)
+
+# Save a feather file
+
