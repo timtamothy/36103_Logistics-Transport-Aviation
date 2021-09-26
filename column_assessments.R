ec0ca1d57f4b70fc792a60e8b433e8636d444696
timothywallaby <stevetimothy@gmail.com>
2021-09-27 08:45:04 +1000

    Plotting individual variables

--- column_assessments.R
+++ column_assessments.R
@@ -13,6 +13,9 @@ allmonths <- read_feather(here('clean_3airline_fleet_employ_2.feather'))
 
 # Overall status
 status(allmonths)
+summary_all <-  summary(allmonths)
+summary_all <- as.data.frame(summary_all)
+write_csv(summary_all, here('column_summary_statistics.csv'))
 
 # Categorical overall
 freq(allmonths, path_out = here('Column EDA', '.'))
@@ -76,3 +79,121 @@ plot_num(num_col_norm)
 status(num_col_norm)
 status(num_col)
 status(allmonths)
+
+# Individual Column Assessment
+names(allmonths)
+
+# Determine Outlier range for *delays only 1 min and greater*
+onlydelay <- allmonths %>% 
+  filter(dep_delay > 0)
+Q3 <- quantile(onlydelay$dep_delay_new, .75)
+IQR <- IQR(onlydelay$dep_delay_new)
+Outlier <- Q3 + 1.5*IQR
+Outlier
+Q3
+
+# Must use a package to make plotting faster
+# filter out outliers of delays
+allmonth_plot <- na.omit(allmonths)
+allmonth_plot <- allmonth_plot %>% 
+  filter(dep_delay <= 95)
+
+# Year Data
+allmonth_plot %>% 
+  sample_n(50000) %>% 
+  ggplot(aes(x=dep_delay, y=year)) +
+  geom_point(alpha=0.3)
+
+# Month Column
+allmonth_plot %>% 
+  sample_n(50000) %>% 
+  ggplot(aes(x=dep_delay, y=month)) +
+  geom_boxplot(alpha=0.2) +
+  geom_point(alpha=0.3)
+
+# Air-Time Column
+allmonth_plot %>% 
+  sample_n(50000) %>% 
+  ggplot(aes(y=dep_delay, x=air_time)) +
+  geom_hex() +       
+  scale_fill_viridis_c() +
+  geom_point(shape = '.', col = 'white')
+  #need to remove air_time that is 0
+
+# Distance Column
+allmonth_plot %>% 
+  sample_n(50000) %>% 
+  ggplot(aes(y=dep_delay, x=distance)) +
+  geom_hex() +       
+  scale_fill_viridis_c() +
+  geom_point(shape = '.', col = 'white')
+  # Check correlation to Air Time
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
