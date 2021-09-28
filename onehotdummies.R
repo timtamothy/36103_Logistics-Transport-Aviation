6df83c9f6fb0017a2cedb251c95bf2c06a7315c4
Charlotte Tse <charlottetse33@gmail.com>
2021-09-28 15:27:46 +1300

    added num transformation

--- onehotdummies.R
+++ onehotdummies.R
@@ -15,11 +15,14 @@ library(future.apply)
 library(httr)
 library(jsonlite)
 library(caret)
-
+plan(multisession)
 # Load data
-
 hot <- read_feather(here('mlm_dataset_3.feather'))
 
+plan(multisession)
+hot <- read_feather("C:/Users/tsetc/Downloads/mlm_dataset_3.feather")
+airports <- read_csv("https://raw.githubusercontent.com/timothywallaby/36103_Logistics-Transport-Aviation/main/top50.csv")
+  
 # Select airports
 airports <- fread(here('top50.csv'))
 colnames(airports)
@@ -35,6 +38,43 @@ hot_filtered <- hot %>%
 unique(hot_filtered$dest_airport_code)
 drop_na(hot_filtered)
 write_feather(hot_filtered, here('top50.feather'))
+
+# Charlotte add (add data trimming and normalization)
+hot_filtered$id <- seq.int(nrow(hot_filtered))
+
+num_col <- select_if(hot_filtered, is.numeric)
+
+num_col <- num_col %>% drop_na()
+
+#i in seq_along(num_col)
+
+num_col <- num_col %>% 
+  select('dep_delay', 'dep_delay_new', 'taxi_out', 'taxi_in',
+         'arr_delay', 'arr_delay_new', 'air_time', 'distance',
+         'age', 'carrier_delay', 'weather_delay', 'nas_delay',
+         'security_delay', 'late_aircraft_delay', 'total_add_gtime',
+         'empfull', 'emppart', 'emptotal', 'empfte')
+
+for (i in 1:9) {
+  if (max(num_col[,i]) > (mean(unlist(num_col[,i])) + 4* sd(unlist(num_col[,i])))) {
+    num_col <- num_col %>% arrange(num_col[,i])
+    num_col <- num_col[round(nrow(num_col) *0.01, digits = 0):round(nrow(num_col)* (1-0.01) , digits = 0),]
+  }
+}
+
+#Min-max transformation 
+min_max_norm <- function(x){
+  (x-min(x)) / (max(x) - min(x))
+}
+num_col_norm <- as.data.frame(future_lapply(num_col, min_max_norm))
+
+cat_col <- hot_filtered %>% select_if(negate(is.numeric))
+cat_col$id <- seq.int(nrow(hot_filtered))
+
+test <- left_join(num_col_norm, cat_col, by = 'id')
+
+hot_filtered <- test %>% select(-id)
+
 # make dummies
 hot_filtered <- hot_filtered %>% 
   select(-arr_delay)
@@ -44,6 +84,7 @@ colnames(hot_filtered)
 dmy <- dummyVars(" ~ . ", data=hot_filtered, fullRank = T)
 dat_transformed <- data.frame(predict(dmy, newdata = hot_filtered))
 
+########
 transform <- function(dummy=dmy, original=hot) {
   transformed_data <- data.frame(predict(dummy, newdata = original))
   return(transformed_data)
@@ -54,6 +95,8 @@ hot_2 <- future_sapply()
 colnames(dat_transformed)
 
 write_feather(dat_transformed, here('dummy.feather'))
+
+write_feather(dat_transformed, here('dummy_numtransform.feather'))
 
 fwrite(dat_transformed, here('dummy.csv'))
 
