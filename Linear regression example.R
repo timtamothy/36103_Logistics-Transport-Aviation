3ea4edfa72ac926e66e4640982aa65d74f056f46
Mandowara Devesh <dmandowara@woolworths.com.au>
2021-09-29 13:21:29 +1000

    Linear Regression exploration on mlm_dataset_3.feather dataset
    Summary of one of the outcomes of the model

--- Linear regression example.R
+++ Linear regression example.R
@@ -1,12 +1,53 @@
-mlm_final <- read_feather("C:/Users/tsetc/Downloads/mlm_dataset_3.feather")
+library(tidyverse)
+library(here)
+library(readr)
+library(future.apply)
+library(dplyr)
+library(httr)
+library(jsonlite)
+library(RColorBrewer)
+library(scales)
+library(feather)
+library(caret)
 
-#filter variables/ subset it 
+
+mlm_final <- read_feather(here('Dataset', 'mlm_dataset_3.feather' ))
+
+#filter variables/ subset it - Think of reasons why these subset variables 
+
+mlm_CA <- mlm_final %>% filter(origin_state_abr == 'CA')
+
+mlm_CA_sub1 <- mlm_CA %>% select(-origin_state_abr)
+
+mlm_CA_sub2 <- mlm_CA_sub1 %>% select(dep_delay, origin_airport_code, dest_airport_code)
+
+# drop levels to include only CA filter
+mlm_CA_sub2 <- droplevels(mlm_CA_sub2)
+
+str(mlm_CA_sub2)
+
+cor(mlm_CA$dep_delay, mlm_CA$month)
+
+lm()
+
+
+# Chi Square test
+data_chi <- data.frame(mlm_CA$manufacturer, mlm_CA$airline)
+data_chi <- table(mlm_CA$manufacturer, mlm_CA$airline)
+
+chisq.test(data_chi)
+
+data_chi_w <- data.frame(mlm_final$manufacturer, mlm_final$airline)
+data_chi_w <- table(mlm_final$manufacturer, mlm_final$airline)
+
+chisq.test(data_chi_w)
+
 
 
 #Trim data + min-max transformation 
-mlm_final$id <- seq.int(nrow(mlm_final))
+mlm_CA_sub2$id <- seq.int(nrow(mlm_CA_sub2))
 
-num_col <- select_if(mlm_final, is.numeric)
+num_col <- select_if(mlm_CA_sub2, is.numeric)
 
 num_col <- num_col %>% drop_na()
 
@@ -23,14 +64,20 @@ min_max_norm <- function(x){
 }
 num_col_norm <- as.data.frame(future_lapply(num_col, min_max_norm))
 
-cat_col <- mlm_final %>% select_if(negate(is.numeric))
-cat_col$id <- seq.int(nrow(mlm_final))
+cat_col <- mlm_CA_sub2 %>% select_if(negate(is.numeric))
+cat_col$id <- seq.int(nrow(mlm_CA_sub2))
 
-mlm_final <- left_join(num_col_norm, cat_col, by = 'id')
+mlm_CA_sub2 <- left_join(num_col_norm, cat_col, by = 'id')
 
 #dummy variable 
-dmy <- dummyVars(" ~ . ", data=mlm_final, fullRank = T)
-dat_transformed <- data.frame(predict(dmy, newdata = mlm_final))
+dmy <- dummyVars(" ~ . ", data=mlm_CA_sub2, fullRank = T)
+dat_transformed <- data.frame(predict(dmy, newdata = mlm_CA_sub2))
 
 #fit data 
 mlm1 <- lm(dep_delay ~ ., data = dat_transformed)
+
+summary_txt <- summary(mlm1)
+
+sink('second_summary.txt')
+summary_txt
+sink()
